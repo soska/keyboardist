76fac60a49f5ac65cc9640f0f83d60ba9238a6d7
Armando Sosa <asosa@nolimit-studio.com>
2018-06-01 17:36:56 -0500

    new docs demo

--- dev/webpack.config.js
+++ dev/webpack.config.js
@@ -20,6 +20,19 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
--- src/docs/index.js
+++ src/docs/index.js
@@ -1,5 +1,52 @@
-import Keyboardist from '../lib';
+// import cover from '../../assets/cover.png';
+import { highlight, languages } from 'prismjs';
+import createListener from '../lib';
 import './styles.css';
+import 'prismjs/themes/prism-okaidia.css';
 
-console.log(Keyboardist);
+// document.getElementById('cover').src = cover;
 
+const listener = createListener();
+
+const pressKey = keyName => {
+  const key = document.querySelector(`.key.${keyName}`);
+  key.classList.add('pressed');
+  window.setTimeout(() => {
+    key.classList.remove('pressed');
+  }, 120);
+};
+
+listener.subscribe('space', () => pressKey('space'));
+listener.subscribe('down', () => pressKey('down'));
+listener.subscribe('up', () => pressKey('up'));
+listener.subscribe('left', () => pressKey('left'));
+listener.subscribe('right', () => pressKey('right'));
+listener.subscribe('shift+space', () => pressKey('shiftspace'));
+listener.subscribe('esc', () => pressKey('escape'));
+
+const code = `
+import createListener from 'keyboardist';
+
+const listener = createListener();
+
+const pressKey = keyName => {
+  const key = document.querySelector(\`.key.\$\{keyName}\`);
+  key.classList.add('pressed');
+  window.setTimeout(() => {
+    key.classList.remove('pressed');
+  }, 120);
+};
+
+listener.subscribe('space', () => pressKey('space'));
+listener.subscribe('down', () => pressKey('down'));
+listener.subscribe('up', () => pressKey('up'));
+listener.subscribe('left', () => pressKey('left'));
+listener.subscribe('right', () => pressKey('right'));
+listener.subscribe('shift+space', () => pressKey('shiftspace'));
+listener.subscribe('esc', () => pressKey('escape'));
+
+`;
+
+const codeWrapper = document.getElementById('code');
+const html = highlight(code, languages.javascript, 'javascript');
+codeWrapper.innerHTML = '<pre class="language-javascript">' + html + '</pre>';
