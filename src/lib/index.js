cc3163da21a89221a518a14130b45a6f407d8c0d
Armando Sosa <arm.sosa@gmail.com>
2021-06-24 16:30:14 -0500

    use @jiveworld/minibus instead of internal event management

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,19 +1,22 @@
-import isInputElement from './is-input-element';
-import isInputEvent from './is-input-event';
-import isEventModifier from './is-event-modifier';
-import getKeyEventName from './get-key-event-name';
+import isInputElement from "./is-input-element";
+import isInputEvent from "./is-input-event";
+import isEventModifier from "./is-event-modifier";
+import getKeyEventName from "./get-key-event-name";
+import { getBus } from "@jiveworld/minibus";
 
-const defaultMonitor = eventName => {
-  console.log(':keyboard event:', eventName);
+const defaultMonitor = (eventName) => {
+  console.log(":keyboard event:", eventName);
 };
 
-function createListener(listenForEvent = 'keydown', element = null) {
-  if (typeof window === 'undefined') {
+function createListener(listenForEvent = "keydown", element = null) {
+  const bus = getBus("@minibus/" + listenForEvent);
+  window.bus = bus;
+
+  if (typeof window === "undefined") {
     // not a browser environment?
     return false;
   }
 
-  let __subscriptions = {};
   let __monitor = null;
 
   // default element is the window document
@@ -24,8 +27,15 @@ function createListener(listenForEvent = 'keydown', element = null) {
   // ignore input events, except when the element is an input.
   const ignoreInputEvents = !isInputElement(element);
 
+  function normalize(eventName) {
+    return eventName
+      .toLowerCase() // Subscripton names are lowercased so both 'Shift+Space' and 'shift+space' works.
+      .replace(/\s/, ""); // remove spaces so both 'Shift + Space' and 'shift+space' works.
+  }
+
   // listens to the event and fires the appropiate event subscription
   function handleKeyEvent(event) {
+    console.log(event);
     if (isEventModifier(event)) {
       return;
     }
@@ -34,49 +44,17 @@ function createListener(listenForEvent = 'keydown', element = null) {
       return;
     }
 
-    const eventName = getKeyEventName(event);
-    const listeners = __subscriptions[eventName.toLowerCase()] || [];
-
-    if (typeof __monitor === 'function') {
-      const matched = listeners.length > 0;
-      __monitor(eventName, matched, event);
-    }
-
-    if (listeners.length) {
-      event.preventDefault();
-    }
-
-    // flag to tell if execution should continue;
-    let propagate = true;
-    //
-    for (let i = listeners.length - 1; i >= 0; i--) {
-      if (listeners[i]) {
-        propagate = listeners[i](event);
-      }
-      if (propagate === false) {
-        break;
-      }
-    }
-    // listeners.map(listener => listener());
+    const eventName = normalize(getKeyEventName(event));
+    bus.emit(eventName, event);
   }
 
   // creates a subscription and returns the unsubscribe function;
   function subscribe(name, callback) {
-    // Subscripton names are lowercased so both 'Shift+Space' and 'shift+space' works.
-    name = name.toLowerCase();
-    // remove spaces so both 'Shift + Space' and 'shift+space' works.
-    name.replace(/\s/, '');
-
-    if (typeof __subscriptions[name] === 'undefined') {
-      __subscriptions[name] = [];
-    }
-    __subscriptions[name].push(callback);
-    return {
-      unsubscribe: () => {
-        const index = __subscriptions[name].indexOf(callback);
-        __subscriptions[name].splice(index, 1);
-      },
-    };
+    const subscription = bus.subscribe(normalize(name), (event) => {
+      event.preventDefault();
+      callback();
+    });
+    return subscription;
   }
 
   // alows to set a monitor function that will run on every event
